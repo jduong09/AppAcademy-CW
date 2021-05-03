c521b5e34149d02cc5bfff433607508f2b127063
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-05-03 15:13:20 -0400

    Delete poker directory

--- poker/lib/card.rb
+++
@@ -1,7 +0,0 @@
-class Card
-  attr_reader :suit, :rank, :value
-
-  def initialize(suit, rank, value)
-    @suit, @rank, @value = suit, rank, value
-  end
-end
--- poker/spec/hand_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative "../lib/hand"
-#The logic of calculating pair, three-of-a-kind, two-pair, etc. goes here.
-#Logic of which hand beats which would go here.
-
-describe Hand do
-
-
-end
