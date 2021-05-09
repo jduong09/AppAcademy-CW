b37e41790cd032e99971f51219fcafb6737223fb
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-05-09 16:18:55 -0400

    Classwork on myhashmap

--- W3D4/myhashmap/lib/p02_hashing.rb
+++ W3D4/myhashmap/lib/p02_hashing.rb
@@ -4,11 +4,32 @@ end
 
 class Array
   def hash
+    each_with_index.inject(0) do |intermediate_hash, (el, i)|
+      (el.hash + i.hash) ^ intermediate_hash
+    end
+    #return 0.hash if self.empty?
+    #bit = nil
+
+    #each do |ele|
+      #num = ele.hash % length
+      
+      #bit = num if bit == nil
+
+      #bit ^ num
+    #end
+
+    #bit
   end
 end
 
 class String
   def hash
+    alphabet = ("a".."z").to_a
+
+    array_of_string = self.split("")
+    char_index = array_of_string.map { |char| alphabet.find_index(char.downcase) }
+    
+    char_index.hash
   end
 end
 
@@ -16,6 +37,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end
