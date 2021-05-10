1b68d54a996d0f781d59b0d5e079c365e35c2ebf
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-05-10 13:50:27 -0400

    Finished LinkList spec

--- W3D4/myhashmap/lib/p03_hash_set.rb
+++ W3D4/myhashmap/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_accessor :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,27 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self[key] << key
+    self.count += 1
+    resize! if num_buckets < self.count
+
+    true
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self.count -= 1 if self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +35,10 @@ class HashSet
   end
 
   def resize!
+    old_store = self.store
+    self.count = 0
+    self.store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.flatten.each { |num| insert(num) }
   end
 end
--- W3D4/myhashmap/lib/p04_linked_list.rb
+++ W3D4/myhashmap/lib/p04_linked_list.rb
@@ -16,11 +16,20 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
   end
 end
 
 class LinkedList
+  include Enumerable
+  attr_reader :head, :tail
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +38,69 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    if @head.next == @tail && @tail.prev == @head
+      return true
+    else
+      return false
+    end
   end
 
   def get(key)
+    each do |node|
+      if node.key == key
+        return node.val
+      end
+    end
+    return nil
   end
 
   def include?(key)
+    return true if get(key) != nil
+    return false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    last_node = @tail.prev
+    last_node.next = new_node
+    @tail.prev = new_node
+    new_node.next = @tail
+    new_node.prev = last_node
+    nil
   end
 
   def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+      end
+    end
+    nil
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.remove
+      end
+    end
+    nil
   end
 
   def each
+    current_node = self.head.next
+    until current_node == self.tail
+      yield current_node
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
