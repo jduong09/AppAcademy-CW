9c11957ee598bd5141fc1a3a74bd32c8afa72a6e
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-06-10 15:17:47 -0400

    Routes file for Art_shares project

--- W7D1/art_share/config/routes.rb
+++ W7D1/art_share/config/routes.rb
@@ -2,9 +2,31 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :destroy, :show, :update, :index] do 
     resources :artworks, only: [:index]
+    resources :comments, only: [:index]
   end
 
-  resources :artworks, only: [:create, :destroy, :show, :update]
+  resources :artworks, only: [:create, :destroy, :show, :update] do
+    resources :comments, only: [:index]
+    member do 
+      post :like, to: "artworks#like", as: "like"
+      post :unlike, to: "artworks#unlike", as: "unlike"
+      post :favorite, to: "artworks#favorite", as: "favorite"
+      post :unfavorite, to: "artworks#unfavorite", as: "unfavorite"
+    end
+  end
 
-  resources :artwork_shares, only: [:create, :destroy]
+  resources :artwork_shares, only: [:create, :destroy] do 
+    member do 
+      post :favorite, to: "artwork_shares#favorite", as: "favorite"
+      post :unfavorite, to: "artwork_shares#unfavorite", as: "unfavorite"
+    end
+  end
+
+  resources :comments, only: [:index, :create, :destroy] do
+    member do 
+      post :like, to: "comments#like", as: "like"
+      post :unlike, to: "comments#unlike", as: "unlike"
+    end
+  end
+
 end
