8e7884463985acb4a3b681bb3c62f7d34875889b
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-06-21 20:39:52 -0400

    MusicApp
    Files that I've written lines of code for in rails project "Music App"

---
+++ W9D1/music_app_git/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  #SessionsController
+  #create should re-set the appropriate user's 
+  #session_token and session[:session_token].
+  #log_in
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      render :new
+    else
+      login!(user)
+      redirect_to user_url(user)
+    end
+  end
+
+  #log_out
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+end
