9b0b2f3eef901498d7aefbb32d702066478fefbc
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-06-27 22:24:50 -0400

    Finished Music_App controllers, views, models.
    Need to style

--- W9D1/music_app_git/controllers/application_controller.rb
+++ W9D1/music_app_git/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   helper_method :current_user
 
   def login!(user)
@@ -14,5 +16,18 @@ class ApplicationController < ActionController::Base
   def current_user
     return nil if session[:session_token].nil?
     @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_session_url
+    end
   end
 end
