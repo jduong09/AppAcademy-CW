16ce30c27ec0b9b62cd1335d0051970a64b1d4d5
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-08-22 16:28:04 -0400

    Add files via upload

--- 08_20/jquery_lite/src/dom_node_collection.js
+++ 08_20/jquery_lite/src/dom_node_collection.js
@@ -4,4 +4,123 @@ class DomNodeCollection {
   }
 };
 
+DomNodeCollection.prototype.html = function html(string) {
+  // if it receives an argument, 
+  //then the argument will become the inner html of each of the html elements.
+  if (string) {
+    this.htmlElements.forEach(htmlElement => {
+      htmlElement.innerHTML = string;
+    });
+    return this.htmlElements;
+  } else {
+    // if it does not receive an argument, 
+    //then it should return the innerHTML of the first element 
+    //in the html elements array.
+    return this.htmlElements[0].innerHTML;
+  }
+};
+
+DomNodeCollection.prototype.empty = function empty() {
+  this.htmlElements.forEach(htmlElement => {
+    htmlElement.innerHTML = "";
+  });
+  return this.htmlElements;
+};
+
+// Insert content, specified by the parameter, to the end of each element in the set of matched elements.
+// This method should accept either a jQuery-lite wrapped collection, an HTML element, or a string.
+// Append the outerHTML of each element in the argument to the innerHTML of each element in the DOMNodeCollection
+// Don't worry about converting strings into HTML elements; just pass them straight through to the elements' innerHTML
+DomNodeCollection.prototype.append = function append(content) {
+  this.htmlElements.forEach(htmlElement => {
+    htmlElement.innerHTML += content;
+  });
+  return this.htmlElements;
+};
+
+//given a string parameter, gets the attribute value at the given attribute name for only the first element in the match set
+//As of jQuery 1.6, the .attr() method returns undefined for attributes that have not been set. 
+//To retrieve and change DOM properties such as the checked, selected, or disabled state 
+//of form elements, use the .prop() method.
+DomNodeCollection.prototype.attr = function attr(attrName, value) {
+  if (arguments.length === 1) {
+    return this.htmlElements[0].getAttribute(attrName);
+  } else {
+    this.htmlElements.forEach(htmlElement => {
+      htmlElement.setAttribute(attrName, value);
+    });
+    return this.htmlElements;
+  }
+};
+
+// Description: Adds the specified class(es) to each element in the set of matched elements.
+DomNodeCollection.prototype.addClass = function addClass(classes) {
+  const classesArray = classes.split(" ");
+  for (let i = 0; i < classesArray.length; i++) {
+    this.htmlElements.forEach(htmlElement => {
+      htmlElement.classList.add(classesArray[i]);
+    });
+  };
+  return this.htmlElements;
+};
+
+DomNodeCollection.prototype.removeClass = function removeClass(classes) {
+  const classesArray = classes.split(" ");
+  for (let i = 0; i < classesArray.length; i++) {
+    this.htmlElements.forEach(htmlElement => {
+      htmlElement.classList.remove(classesArray[i]);
+    });
+  };
+  return this.htmlElements;
+};
+
+//Traversal
+//children is a method that should return a DOMNodeCollection of ALL children of all nodes in the array
+//Each node in the array will natively have a children attribute. Look here for more information.
+//Make sure the return value of this method is an instance of DOMNodeCollection.
+
+DomNodeCollection.prototype.children = function children() {
+  const allChildren = [];
+  this.htmlElements.forEach(htmlElement => {
+    const children = htmlElement.children;
+    allChildren.push(children);
+  });
+  return new DomNodeCollection(allChildren);
+};
+
+//Return a DOMNodeCollection of the parents of each of the nodes
+DomNodeCollection.prototype.parent = function parent() {
+  const allParents = [];
+  for(let i = 0; i < this.htmlElements.length; i++) {
+    const element = this.htmlElements[i];
+    allParents.push(element.parentNode);
+  };
+  return new DomNodeCollection(allParents);
+};
+
+//Returns a DOMNodeCollection of all the nodes matching the selector 
+//passed in as an argument that are descendants of the nodes. 
+//This might come in handy.
+DomNodeCollection.prototype.find = function find(selector) {
+  const results = [];
+  this.htmlElements.forEach(htmlElement => {
+    const elements = htmlElement.querySelectorAll(selector);
+    for (let i = 0; i < elements.length; i++) {
+      results.push(elements[i]);
+    }
+  })
+  return results;
+};
+
+DomNodeCollection.prototype.remove = function remove() {
+  const removedElements = [];
+  this.htmlElements.forEach(htmlElement => {
+    removedElements.push(htmlElement)
+    htmlElement.remove();
+  });
+  return removedElements;
+}
+
+//Phase 2: Event Handling
+
 module.exports = DomNodeCollection;
--- 08_20/jquery_lite/src/index.js
+++ 08_20/jquery_lite/src/index.js
@@ -1,19 +1,28 @@
 const DomNodeCollection = require("./dom_node_collection");
 
+// jquery's .jquery() function
+// when passed a function, it runs the function when the document is ready.
+// when passed a string
+  // jquery examines if it looks like html, jquery attempts to create new DOM elements as described by the HTML
+  // if not, the string is interpreted as a selector expression (ex: $("div")) should return an array with all <div>'s from the DOM
+// when passed an object
+  // ..
 
-function $1(selector) {
-  const array = [];
-  if (selector instanceof Element) {
-    array.push(selector)
-    return new DomNodeCollection(array);
+window.$1 = (arg) => {
+  switch (typeof arg) {
+    case "string":
+      return getNodesFromDom(arg);
+    case "object": 
+      if (arg instanceof HTMLElement) {
+        return new DomNodeCollection([arg]);
+      }
   }
-  const nodeList = document.querySelectorAll(selector);
-  nodeList.forEach((li) => {
-    array.push(li);
-  });
-  return new DomNodeCollection(array);
 };
 
+getNodesFromDom = (selector) => {
+  const nodes = document.querySelectorAll(selector);
+  const nodesArray = Array.from(nodes);
+  return new DomNodeCollection(nodesArray);
+};
 
-window.$1 = $1;
 
