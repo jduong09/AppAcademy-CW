8c48b86cefab645310d7be1b05044dc5b8ef2d4f
jduong09 <59810054+jduong09@users.noreply.github.com>
2021-09-15 20:08:19 -0400

    Created a rails project for ToDos.
    Created a database, created controllers and linked redux/react frontend to rails project.
    Next, add AJAX request from front end to make changes in the backend.

---
+++ 09_15/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.create({ "title" => "Wash my car", "body" => "Wash the Honda by Sunday", "done" => false })
+Todo.create({ "title" => "Food Prep", "body" => "Make lunch/dinner for the week", "done" => false })
+Todo.create({ "title" => "Practice CSS", "body" => "Make websites and practice styling them", "done" => false })
